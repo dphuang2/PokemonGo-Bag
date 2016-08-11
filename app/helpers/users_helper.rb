248e58d1ee9b0026d22c054eccc2d3493e8df486
Ibara Mayaka <ibaramayaka12345@gmail.com>
2016-08-11 15:37:57 -0600

    Added types to bag using JSON list

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,28 @@ module UsersHelper
     number = format("%03d", number)
     image_tag("pokemon/"+/[0-9]{3}.{10}.png/.match(Dir["app/assets/images/pokemon/#{number}*"][0]).to_s) 
   end
+  
+  def pokemon_image_type1(number)
+    file = File.read('app/assets/pokemon.types.json')
+    pokemon_hash = JSON.parse(file)
+	type = pokemon_hash[number.to_s]['type_1']
+	
+	if (type == 'none')
+		return ""
+	end
+	
+    return type
+  end
+  
+  def pokemon_image_type2(number)
+    file = File.read('app/assets/pokemon.types.json')
+    pokemon_hash = JSON.parse(file)
+	type = pokemon_hash[number.to_s]['type_2']
+	
+	if (type == 'none')
+		return ""
+	end
+	
+    return type
+  end
 end
