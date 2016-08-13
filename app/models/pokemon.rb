8466444622af1763598eda296770f8ead4f788bf
Ibara Mayaka <ibaramayaka12345@gmail.com>
2016-08-13 07:20:14 -0600

    Types stored into db

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,6 +1,8 @@
 class Pokemon < ApplicationRecord
   belongs_to :user, dependent: :destroy
   validates :poke_id, presence: true
+  validates :type_1, presence: true
+  validates :type_2, presence: true
   validates :move_1, presence: true
   validates :move_2, presence: true
   validates :max_health, presence: true
